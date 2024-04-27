9ec2024c234713ad424feed02c66970260cf5568
sumitsaini01 <sumitsaini8710@gmail.com>
2024-04-27 15:51:45 +0530

    first commit

---
+++ hguds.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public
+    ListNode deleteDuplicates(ListNode head)
+    {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+        /*Name = Sumit Saini 
+        UID = 21BCS11504*/
+        while (head != null)
+        {
+            while (head.next != null && head.val == head.next.val)
+                head = head.next;
+            if (prev.next == head)
+                prev = prev.next;
+            else
+                prev.next = head.next;
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
